e089d17f890d264cb46d40ab447e0feed88870e9
shreyashe <shreyash.22010681@viit.ac.in>
2022-07-20 22:37:08 +0530

    navbar function added

--- src/components/Header/Responsive_navbar.js
+++ src/components/Header/Responsive_navbar.js
@@ -9,10 +9,10 @@ const Responsive_navbar = ({RemoveShowMenu}) => {
 
         <ul className='Res_Navlinks'>
         <li onClick={RemoveShowMenu}><ImCross/></li>
-              <li><Link exact  to="/">Home</Link></li>
-              <li><Link exact  to="/menu">Menu</Link></li>
-              <li><Link exact  to="/about">About</Link></li>
-              <li><Link exact  to="/reviews">Reviews</Link></li>
+              <li  onClick={RemoveShowMenu}><Link exact  to="/">Home</Link></li>
+              <li onClick={RemoveShowMenu}><Link exact  to="/menu">Menu</Link></li>
+              <li onClick={RemoveShowMenu}><Link exact  to="/about">About</Link></li>
+              <li onClick={RemoveShowMenu}><Link exact  to="/reviews">Reviews</Link></li>
             </ul>
             </div>
     
